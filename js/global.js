98342ced26d6e9fd5da2b1f58e95fd570083e55a
Thomas Gouty <goutybattlenet@gmail.com>
2018-02-16 17:18:09 +0100

    integration xml

--- js/global.js
+++ js/global.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
 	var myPassWord = "root"
 	console.log("hello")
 	
+	lireHistorique();
+	$("#envoyer_message").click(envoyerMessage);
+	
 	//Check auth
 	$("button#validateID").on("click",function(){
 		console.log("myLogin")
@@ -42,4 +45,53 @@ $(document).ready(function(){
 				ajouterMessageDansXML(result);
 			}, "xml");
 		}
+		
+function lireHistorique()
+{
+	$.post("xml/xmlChat.xml", function(result){
+		//var r = $(result).find("status");
+		traiteHistorique(result);
+	}, "xml");
+}
+
+
+function traiteHistorique(Xreponse)
+	{
+		var Xstatus = $(Xreponse).find('message').each(function(){
+			var idChannel = $(this).find('idChannel').text();
+			var dateTime = $(this).find('dateTime').text();
+			var idPseudo = $(this).find('idPseudo').text();
+			var message = $(this).find('message').text();
+
+			$("p#historique_message").append("<p  class=\"btn-primary\">" +  message + "</p>");
+
+		});
+	}
+
+
+function envoyerMessage(){
+		var message = $("#message").val();
+		alert($.now());
+
+		$("p#historique_message").append("<p  class=\"btn-primary\">" +  message + "</p>");
+
+		
+		$("#message").val("");
+
+		$.post("xml/xmlChat.xml", function(result){
+
+			ajouterMessageDansXML(result);
+		
+		}, "xml");
+
+}
+
+function ajouterMessageDansXML(xmlDoc){
+			
+
+		var xmlDoc2 = $(xmlDoc).find('messages');
+		xmlDoc2.append('<message>' + "1" +'</message>');/*
+			$(Xreponse).find('message').append('<idChannel>' + "principal" +'</idChannel>');
+			$(Xreponse).find('message').append('<dateTime>'+ '1518795516807' +'</dateTime>');*/
+}
 });
